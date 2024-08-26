6ebf12060635e2fccb4c571c607cd8fb64a8b1a5
BastD29 <bastiendaniel9@gmail.com>
2024-08-26 15:43:34 +0200

    add createTodo controller

--- server/src/controllers/todo.ts
+++ server/src/controllers/todo.ts
@@ -4,13 +4,36 @@ import { TodoType } from "../types/todo";
 
 const createTodo = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const { title, description } = req.body;
+
+    if (!title || !description) {
+      return res
+        .status(400)
+        .send({ message: "Title and description are required" });
+    }
+
+    const createdRecord = await base("Table 1").create({
+      Title: title,
+      Description: description,
+    });
+
+    const newTodo: TodoType = {
+      id: createdRecord.id as unknown as number,
+      title: createdRecord.fields.Title as string,
+      description: createdRecord.fields.Description as string,
+    };
+
+    res.status(201).send({ message: "Todo created successfully", newTodo });
+  } catch (error) {
+    console.error((error as Error).message);
+    res.status(500).send({ message: "Failed to create todo", error });
+  }
 };
 
 const getTodos = async (req: Request, res: Response) => {
   try {
     const todos: TodoType[] = [];
-    console.log("todos:", todos);
+    // console.log("todos:", todos);
 
     await base("Table 1")
       .select({ view: "Grid view" })
@@ -21,7 +44,7 @@ const getTodos = async (req: Request, res: Response) => {
             title: record.get("Title") as string, // Assuming the Airtable field is named "Title"
             description: record.get("Description") as string, // Assuming the Airtable field is named "Description"
           };
-          console.log("todo:", todo);
+          //   console.log("todo:", todo);
 
           todos.push(todo);
         });
