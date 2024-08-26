45b361fa3359b1fe7b7a5b6ba38964be46303097
BastD29 <bastiendaniel9@gmail.com>
2024-08-26 12:38:39 +0200

    airtable env config

--- server/src/config/environments.ts
+++ server/src/config/environments.ts
@@ -4,14 +4,23 @@ const env = process.env.NODE_ENV || "development";
 
 dotenv.config({ path: `./environments/.env.${env}` });
 
-const { NODE_ENV, ALLOWED_ORIGIN, PORT } = process.env as {
-  NODE_ENV: string;
-  ALLOWED_ORIGIN: string;
-  PORT: string;
-};
+const { NODE_ENV, ALLOWED_ORIGIN, PORT, AIRTABLE_API_KEY, AIRTABLE_BASE_ID } =
+  process.env as {
+    NODE_ENV: string;
+    ALLOWED_ORIGIN: string;
+    PORT: string;
+    AIRTABLE_API_KEY: string;
+    AIRTABLE_BASE_ID: string;
+  };
 
-if (!NODE_ENV || !ALLOWED_ORIGIN || !PORT) {
+if (
+  !NODE_ENV ||
+  !ALLOWED_ORIGIN ||
+  !PORT ||
+  !AIRTABLE_API_KEY ||
+  !AIRTABLE_BASE_ID
+) {
   throw new Error("environment variables are not properly defined");
 }
 
-export { NODE_ENV, ALLOWED_ORIGIN, PORT };
+export { NODE_ENV, ALLOWED_ORIGIN, PORT, AIRTABLE_API_KEY, AIRTABLE_BASE_ID };
--- server/src/types/dotenv.d.ts
+++ server/src/types/dotenv.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
     NODE_ENV: "development" | "production" | "test" | "local" | "staging";
     PORT: string;
     ALLOWED_ORIGIN: string;
+    AIRTABLE_API_KEY: string;
+    AIRTABLE_BASE_ID: string;
   }
 }
